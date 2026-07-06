d23ba79502e391492ee95c84631d39b13a9808ef
Anthony Krieg <akriegr@ucenfotec.ac.cr>
2026-07-05 21:27:21 -0600

    Clase - DAO
    Clase Categoria inicializada y dao en proceso

---
+++ Categoria.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+class Categoria {
+private: 
+	//atributos 
+	int id;
+	string nombre;
+public:
+	//constructores 
+	Categoria() : id(0), nombre("") {} // primero vacia
+	Categoria(int id, const string& nombre) : id(id), nombre(nombre) {} // segunda con parametros
+	
+	//getters y setters
+	int getId() const { return id; }
+	void setId(int id) { this->id = id; }
+	string getNombre() const { return nombre; }
+	void setNombre(const string& nombre) { this->nombre = nombre; }
+
+
+
+};
---
+++ ICategoriaDAO.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Categoria.h"
+#include <vector>
+#include <optional>
+
+class ICategoriaDAO {
+	//interfaz de categoriaDAO, defino cuales operaciones se pueden hacer 
+public:
+	virtual ~ICategoriaDAO() = default;
+
+	//Crud basico
+	virtual bool insertar(const Categoria& categoria) = 0;
+	virtual optional<Categoria> obtenerPorId(int id) = 0;
+	virtual vector <Categoria>ObtenerCategorias() = 0;
+	virtual bool actualizar(const Categoria& categoria) = 0;
+	virtual bool eliminar(int id) = 0;	
+
+	//Consultas adicionales
+
+
+
+
+
+
+};
